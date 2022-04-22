707b1c2721dbbebc4e274d86e86dbf17b3a7e2eb
ryanoliveiragit <ryanoliveirabrito178@gmail.com>
2022-04-21 22:40:47 -0300

    New version
    Modificado:
    assets
    index
    style.css
    
    Adicionados:
    script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function altera_tamanho(){
+    /*seta=document.getElementById("seta");
+    if(seta.style.height =="30px")
+        seta.style.height = 270 + 'px';
+    else
+        seta.style.height = 30 + 'px';*/
+        alert('Portfolio em desenvolvimento')
+}
+
+function altera_tamanho2(){
+    /* 
+    seta=document.getElementById("ppasta");
+    if(seta.style.height =="50%")
+        seta.style.height = 330 + 'px';
+    else
+        seta.style.height = 50 + '%';
+        */
+        alert('Portfolio em desenvolvimento')
+}
